28ec8f5b7bd0dd390dbb6d43dc2d209277ef7535
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-04-24 20:37:19 -0500

    current config entity

---
+++ invoper-cli/Entities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace invoper_cli
+{
+    public class Entities
+    {
+        public class Configuration
+        {
+            public string IsoName;
+            public string File;
+            public string LanguageName;
+        }
+
+    }
+}
